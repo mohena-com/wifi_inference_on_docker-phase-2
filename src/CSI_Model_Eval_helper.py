3b74ad73de56d742899e5dde21411536b39d9923
Sanjeev Pandey <urssanj00@users.noreply.github.com>
2025-10-28 21:58:42 +0530

    config

--- src/CSI_Model_Eval_helper.py
+++ src/CSI_Model_Eval_helper.py
@@ -32,7 +32,7 @@ from DL_DenseNet1D import DenseNet1D
 from DL_EfficientNet1DLSTM import EfficientNet1DLSTM
 from DL_MobileNetV3 import MobileNetV3_1D_LSTM
  
-cr = ConfigReader("../config/har_infer_config.properties")
+cr = ConfigReader("config/har_infer_config.properties")
 
 def parse_run_name(run_name: str) -> dict:
     """
