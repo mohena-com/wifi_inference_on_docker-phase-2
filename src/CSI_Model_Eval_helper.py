344132ab3462b9fd5f13bca6a25d99b93f5c01df
Sanjeev Pandey <urssanj00@users.noreply.github.com>
2025-11-02 02:35:20 +0530

    WifiCSIDataset

--- src/CSI_Model_Eval_helper.py
+++ src/CSI_Model_Eval_helper.py
@@ -209,10 +209,11 @@ def get_best_model_and_params(best_model_fname=None):
         print(f"Failed to instantiate model: {e}")
     return model_instance, params, total_params, device
 
+'''
 run_name = "best_overall_model_final_MobileNetV3_1D_LSTM_lr5e-04_bs16_adam_wd1e-04_ep100_valacc0.9656.pt"
 get_best_model_and_params(run_name)
 
 
 dataset = WifiCSIDataset(logger, filelist, window_size=128, stride=64)
-
+'''
 
