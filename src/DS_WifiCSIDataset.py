dbc705d02fd439b8a7f896f5c83363c54317ebc9
Sanjeev Pandey <urssanj00@users.noreply.github.com>
2025-11-02 21:50:10 +0530

    window_size

--- src/DS_WifiCSIDataset.py
+++ src/DS_WifiCSIDataset.py
@@ -65,6 +65,9 @@ class WifiCSIDataset(Dataset):
             print(f"A_01_{i}. y: {y} ")
             print(f"A_01_window_size:{window_size}. stride: {stride}. T: {T}")
             
+            if T < window_size:
+                window_size = T  # Adjust window size if sequence is shorter
+                
             for start in range(0, T - window_size + 1, stride):
                 m_seq = X_meta[start:start+window_size]   # (W, 12)
                 csi_seq = X_csi[start:start+window_size]  # (W, 99)
