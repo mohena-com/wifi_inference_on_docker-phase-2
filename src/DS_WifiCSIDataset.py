0e56538564221386dd1b3f4750fda3ab283d405c
Sanjeev Pandey <urssanj00@users.noreply.github.com>
2025-11-02 21:41:45 +0530

    sa_row

--- src/DS_WifiCSIDataset.py
+++ src/DS_WifiCSIDataset.py
@@ -147,7 +147,7 @@ class WifiCSIDataset(Dataset):
                 else:
                     subj.append(s)
                     act.append(a)
-                print(f"B_02_{i}. Row {i} loaded. Subject: {subj[-1]} Activity: {act[-1]}"  )
+                print(f"Row {i} loaded. Subject: {subj[-1]} Activity: {act[-1]}"  )
                     
 
         X_meta = np.array(X_meta, dtype=np.float32)  # (T, 12)
