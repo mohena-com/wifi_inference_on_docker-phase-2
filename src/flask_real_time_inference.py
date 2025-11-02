e2bc1d70066e2d3e8b1ec004d74d2de3a53072a7
Sanjeev Pandey <urssanj00@users.noreply.github.com>
2025-11-03 00:29:22 +0530

    output

--- src/flask_real_time_inference.py
+++ src/flask_real_time_inference.py
@@ -116,12 +116,14 @@ def evaluate_model_on_input_data(test_loader):
             # --- Forward pass ---
             # --- Forward pass (fix order) ---
             outputs = model(csi_seq, meta_seq)
+            for a in outputs:
+                print(f"0==>output :{a}")
             probs = F.softmax(outputs, dim=1)
             preds = torch.argmax(outputs, dim=1)
             print(f"CSI shape: {csi_seq.shape}, META shape: {meta_seq.shape}, outputs: {outputs.shape}")
-            
             for a in outputs:
-                print(f"==>output :{a}")
+                print(f"1==>output :{a}")
+
 
             # --- Always store predictions and probabilities ---
             val_pred.extend(preds.cpu().numpy().tolist())
