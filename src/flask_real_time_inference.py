b5b5a5183e4a43bd49b48e4f7f70207663510701
Sanjeev Pandey <urssanj00@users.noreply.github.com>
2025-11-08 15:41:53 +0530

    Prediction

--- src/flask_real_time_inference.py
+++ src/flask_real_time_inference.py
@@ -142,7 +142,7 @@ def evaluate_model_on_input_data(test_loader, model, device, params=None):
                 correct += (preds == labels).sum().item()
                 print(f"✅  VERIFICATION of Prediction for Batch {batch_idx + 1}")
                 for y, p in zip(labels.squeeze().tolist(), preds.squeeze().tolist()):
-                    print(f"     true:{y}  pred:{p}")
+                    print(f"     true:{y}  pred:{p} = {y == p} Prediction")
                 total += labels.size(0)
                 val_true.extend(labels.cpu().numpy().tolist())
                 print(f"Batch {batch_idx + 1}: loss={loss.item():.4f}, acc={(preds == labels).sum().item()}/{labels.size(0)}")
