5b68765146194df763f224ae6577a7788461f9d2
Sanjeev Pandey <urssanj00@users.noreply.github.com>
2025-11-08 12:20:40 +0530

    del

--- src/flask_real_time_inference.py
+++ src/flask_real_time_inference.py
@@ -308,6 +308,9 @@ def predict():
             results.append(result_entry)
 
         print(f"Prediction complete for {len(saved_file_paths)} file(s).")
+
+        cleanup_files()
+
         return jsonify({
             "message": "Prediction successful",
             "total_files": len(saved_file_paths),
@@ -320,12 +323,13 @@ def predict():
 
 
  
-
+def cleanup_files():
     filelist = glob.glob(os.path.join('/tmp/uploads', '**', '*.csv'), recursive=True)
     print(f"Predict: Found {len(filelist)} CSV files in /tmp/uploads for dataset creation.")  
+     '''
     # Now pass the saved file paths to WifiCSIDataset
     dataset = WifiCSIDataset(logger, filelist, window_size=128, stride=64)
-    
+   
     i = 0
     for a in dataset.samples:        
         m_seq = a[0]
@@ -339,12 +343,12 @@ def predict():
             print(f"-->Label : {l} ") 
         i += 1
 
-    
+   
     print(f"Dataset created with {len(dataset)} samples from uploaded files.")
     
      
     val_true, val_pred, val_prob = evaluate_model_on_input_data(dataset)
-    
+     '''
     # Delete uploaded files after processing
     for file_path in saved_file_paths:
         try:
@@ -352,9 +356,7 @@ def predict():
             print(f"Deleted uploaded file: {file_path}")
         except Exception as e:
             logger.warning(f"Failed to delete file {file_path}: {e}")
-    val_json = create_json_message(val_true, val_pred, val_prob)
-    print(f"Returning JSON: {val_json}")
-    return val_json
+    
 
  
 
