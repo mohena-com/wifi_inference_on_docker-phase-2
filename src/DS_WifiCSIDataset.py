613dd8b95f6be69aaf5c54fa7d9e28233ebe3e64
Sanjeev Pandey <urssanj00@users.noreply.github.com>
2025-11-08 14:23:19 +0530

    len

--- src/DS_WifiCSIDataset.py
+++ src/DS_WifiCSIDataset.py
@@ -38,8 +38,8 @@ class WifiCSIDataset(Dataset):
         for f in file_list:
             X_meta, X_csi, _, _, _ = self.load_csv_as_numpy(f)
             self.logger.debug(f"A_00_{i}. X_meta: {X_meta.shape} X_csi: {X_csi.shape}")
-            print(f"A_00_{i}. X_meta: {X_meta} ")            
-            print(f"A_01_{i}. X_csi: {X_csi}")
+            print(f"A_00_{i}. X_meta: {len(X_meta[0])} ")
+            print(f"A_01_{i}. X_csi: {len(X_csi[0])} ")
 
             all_meta.append(X_meta)
             all_csi.append(X_csi)
