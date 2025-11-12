a3b38f0bc63511c3bf90d22ce43c28a7333fc4ed
Sanjeev Pandey <urssanj00@users.noreply.github.com>
2025-11-13 01:57:20 +0530

    shuffle

--- src/flask_real_time_inference.py
+++ src/flask_real_time_inference.py
@@ -253,7 +253,7 @@ def predict():
         batch_size = int(params.get('batch_size', 8))
         print(f"🧩 Creating test dataset and loader with batch_size={batch_size}")
         test_dataset = WifiCSIDataset(logger=logger, file_list=saved_file_paths, window_size=128, stride=64)
-        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=0)
+        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
 
         # Debug
         logger.info(f"DEBUG: dataset size (len): {len(test_dataset)}")
